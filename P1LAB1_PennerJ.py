e970105f24a81411bb2fdc835ad02938ae02822a
Joetatoes0117 <pennerj0421@student.faytechcc.edu>
2024-09-22 11:44:17 -0400

    Add files via upload

---
+++ P1LAB1_PennerJ.py
@@ -0,0 +1,7 @@
+
+input("Please enter your first name: ")
+input("Please enter your last name: ")
+print("9/22/2024")
+print("P1LAB1 Instructions")
+print("This assignment introduces students to creating a Python program using IDLE. It demonstrates how to receive input from the user and display output effectively.")
+
