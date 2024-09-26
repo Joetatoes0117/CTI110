b8b45f3ccec8d2a5a5dff968eb65eb015f654cd0
Joetatoes0117 <pennerj0421@student.faytechcc.edu>
2024-09-26 14:37:49 -0400

    Add files via upload

---
+++ P1LAB1_PennerJ.py
@@ -0,0 +1,9 @@
+
+# Student: Joseph Penner
+# Date: 9/22/2024
+# Assiagnment: Name P1LAB1
+# Description: This assignment introduces students to creating a Python program using IDLE. It demonstrates how to receive input from the user and display output effectively.")
+first_name = input("Please enter your first name:")
+last_name = input("Please enter your last name:")
+print('Hello',first_name, last_name, 'and welcome to CT1-110')
+
