066fa68d4b4bf2b717320f4b68b01b046dc65092
Joetatoes0117 <pennerj0421@student.faytechcc.edu>
2024-09-29 15:55:25 -0400

    Add files via upload

---
+++ P1HW1_PennerJoseph.py
@@ -0,0 +1,40 @@
+# Student: Joseph Penner
+# Date: 9/26/2024
+# Assiagnment: Name P1HW1
+# Description: Assignment tests student's knowledge of how to write code that collects information from user, processes information collected and display results to user.
+print('-----Calculating Expotents-----')
+
+(print(""))
+
+base = int(input("Enter an integer as the base value:"))
+exponent = int(input("Enter an integer as a exponent:"))
+
+(print(""))
+
+result = base ** exponent
+
+(print(""))
+
+print(f"{base} to the power of {exponent} is {result}!")
+
+(print(""))
+(print(""))
+
+print('-----Addition and Subtraction-----')
+
+(print(""))
+
+num1 = int(input("Enter the startng integer:"))
+num2 = int(input("Enter integer to add: "))
+num3 = int(input("Enter the integer to subtract:"))
+
+(print(""))
+
+sum_of_first_two = num1 + num2
+final_result = sum_of_first_two - num3
+print(f"{num1} + {num2} = {sum_of_first_two} - {num3} is equal to {final_result}.")
+
+
+
+
+
